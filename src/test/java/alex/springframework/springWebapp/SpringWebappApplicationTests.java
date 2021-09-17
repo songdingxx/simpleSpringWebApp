c6da16c1f438eb7a435e7ade972995fb0897735a
songdingxx <songdingxx@gmail.com>
2021-09-16 17:21:52 -0700

    add files to repo

---
+++ src/test/java/alex/springframework/springWebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.springframework.springWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
